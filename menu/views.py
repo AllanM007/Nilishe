bc1b6788c870ae66c3bdda2a58a6ceafa0385386
AllanM007 <mwaranguallan345@gmail.com>
2020-06-11 14:02:30 +0300

    clear sessions

--- menu/views.py
+++ menu/views.py
@@ -27,14 +27,6 @@ def pizza(request):
                 sauce=form.cleaned_data["sauce"],
             )
 			pizza.save()
-
-			size = request.POST.get('size')
-			topping = request.POST.get('topping')
-			sauce = request.POST.get('sauce')
-
-			request.session['test1'] = str(size)
-			request.session['test2'] = str(topping)
-			request.session['test3'] = str(sauce)
 		
 		return redirect('menu:cart')
 
@@ -48,11 +40,6 @@ def pizza(request):
 	return render(request, 'menu/pizza.html', {'form':form})
 
 def cart(request):
-
-	size = request.session['test1']
-	topping = request.session['test2']
-	sauce = request.session['test3']
-	
 	
 	context={
         'size':size,
