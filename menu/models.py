7ae7159ae0d69055e16b70f24b018d8766e6dc38
AllanM007 <mwaranguallan345@gmail.com>
2020-06-14 16:41:15 +0300

    add 6 char hex identifier

--- menu/models.py
+++ menu/models.py
@@ -4,7 +4,7 @@ from django.db import models
 import uuid
 
 def hex_uuid():
-    return uuid.uuid4().hex
+    return uuid.uuid4().hex[:6].upper()
 
 class Pizza(models.Model):
     size = models.CharField(max_length=20, blank=True)
