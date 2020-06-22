abca3c2c90e458b05bf490e060540ae5ff294436
AllanM007 <mwaranguallan345@gmail.com>
2020-06-22 20:48:18 +0300

    edit app.js

--- chat/static/chat/app.js
+++ chat/static/chat/app.js
@@ -10,49 +10,6 @@ const mapSocket = new WebSocket(
 
 uRL = (mapSocket.url)
 
-mapSocket.onopen = function (event) {
-    mapSocket.send(JSON.stringify({
-        'latlng': latlng
-    }));
-
-    console.log(latlng);
-    
-    console.log('map data sent');
-};
-
-mapSocket.onmessage = function(e) {
-    const data = JSON.parse(e.data);
-    console.log(data.latlng.lat, data.latlng.lng);
-
-    var deliveries = [
-        { "lat": location.coords.latitude, "lon": location.coords.longitude},
-        { "lat": data.latlng.lat, "lon": data.latlng.lng},
-    ];
-
-    var marker = (function() {
-    for (let index = 0; index < deliveries.length; index++) {
-        L.marker([deliveries[index].lat, deliveries[index].lon])
-        .bindPopup("Your delivery is within " + radius + " meters of your area")
-        .addTo(mymap);
-        }
-    })();
-
-    L.Routing.control({
-        waypoints: [
-            L.latLng(deliveries[0].lat, deliveries[0].lon),
-            L.latLng(deliveries[1].lat, deliveries[1].lon)
-        ]
-    }).addTo(mymap);
-};
-
-mapSocket.onclose = function(e) {
-    console.error('Map socket closed unexpectedly');
-};
-
-mapSocket.onerror = function(event) {
-    console.error("WebSocket error observed:", event);
-};
-
 var map = L.map('mapid'),
     realtime = L.realtime({
         url: 'https://wanderdrone.appspot.com/',
