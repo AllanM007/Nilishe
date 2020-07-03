061ce822920a3301cceb271c597f85defc85cc9e
AllanM007 <mwaranguallan345@gmail.com>
2020-07-03 20:57:42 +0300

    update

--- nilishe/settings.py
+++ nilishe/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'crispy_forms',
-    'djgeojson',
+    #'djgeojson',
     'payments',
     'leaflet',
     'users',
