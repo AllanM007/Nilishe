1e0ece21f4cd55ff082e6d7e9a16472c68a9ee3c
AllanM007 <mwaranguallan345@gmail.com>
2020-07-04 15:12:43 +0300

    mod settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: daphne nilishe.asgi:application --port $PORT --bind 0.0.0.0
--- nilishe/settings.py
+++ nilishe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u*2ul-c^s1e7sx8d2bo(h#32&@eie3pmjf^d7iucb1s)&xn(i='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
